62d47ab735b361a556ab44195e799a8d5bcb5a7e
Park so yeon <namhui271@gmail.com>
2024-08-18 23:18:43 +0900

    second_router

---
+++ README.md
@@ -0,0 +1,16 @@
+# wetube Reloaded
+
+/ -> home
+/join -> Join
+/login -> login
+/search -> Search video
+
+/user/:id -> See user
+/user/logout -> Log out
+/user/edit -> Edit my profile
+/user/delete -> Delete my profile
+
+/video/:id -> Watch video
+/video/:id/edit -> Edit video
+/video/:id/delete -> Delete video
+/video/upload -> Upload video
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,8 @@
 export const trending = (req, res) => res.send("Home");
-export const watch = (req,res)=>res.send("watch video");
+export const watch = (req,res)=> {
+	res.send(`watch video ${req.params.id}`);
+}
 export const edit = (req,res) => res.send("edit video");
+export const search = (req, res) =>res.send("search video");
+export const remove = (req, res) => res.send("remove video");
+export const upload = (req ,res) => res.send("upload video");
