5d0ca9c6d1fba3958d4bfd6e44aa96ad2e384384
Park so yeon <namhui271@gmail.com>
2024-08-26 20:08:30 +0900

    template_0826

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,10 @@
-export const trending = (req, res) => res.render("home");
-export const watch = (req,res)=> res.render("watch");
+const fakeUser = {
+	username : "parksoyeon",
+	loggedIn : true,
+}
+
+export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser : fakeUser});
+export const watch = (req,res)=> res.render("watch", {pageTitle : "Watch this video"});
 export const edit = (req,res) => res.send("edit video");
 export const search = (req, res) =>res.send("search video");
 export const remove = (req, res) => res.send("remove video");
