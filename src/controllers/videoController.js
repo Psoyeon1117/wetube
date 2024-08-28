cac4e2f6b34a8263cbbe838a34017fede9446c9f
Psoyeon1117 <namhui271@gmail.com>
2024-08-28 19:33:06 +0900

    template_0828

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,8 +2,38 @@ const fakeUser = {
 	username : "parksoyeon",
 	loggedIn : true,
 }
-
-export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser : fakeUser});
+export const trending = (req, res) => {
+	const videos = [
+		{
+		  title: "Hello",
+		  title: "First Video",
+		  rating: 5,
+		  comments: 2,
+		  createdAt: "2 minutes ago",
+		  views: 59,
+		  id: 1,
+		},
+		{
+		  title: "Video #2",
+		  title: "Second Video",
+		  rating: 5,
+		  comments: 2,
+		  createdAt: "2 minutes ago",
+		  views: 59,
+		  id: 1,
+		},
+		{
+		  title: "Whatsup",
+		  title: "Third Video",
+		  rating: 5,
+		  comments: 2,
+		  createdAt: "2 minutes ago",
+		  views: 59,
+		  id: 1,
+		},
+	  ];
+	res.render("home", {pageTitle : "Home", fakeUser : fakeUser, videos});
+}
 export const watch = (req,res)=> res.render("watch", {pageTitle : "Watch this video"});
 export const edit = (req,res) => res.send("edit video");
 export const search = (req, res) =>res.send("search video");
