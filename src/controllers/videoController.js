c80d3900e33217c60413d26f7c70b3840745d12c
Psoyeon1117 <namhui271@gmail.com>
2024-08-29 20:28:48 +0900

    database_0829

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,41 +1,49 @@
-const fakeUser = {
-	username : "parksoyeon",
-	loggedIn : true,
-}
+
+const videos = [
+	{
+	  title: "First Video",
+	  rating: 5,
+	  comments: 2,
+	  createdAt: "2 minutes ago",
+	  views: 1,
+	  id: 1,
+	},
+	{
+	  title: "Second Video",
+	  rating: 5,
+	  comments: 2,
+	  createdAt: "2 minutes ago",
+	  views: 59,
+	  id: 2,
+	},
+	{
+	  title: "Third Video",
+	  rating: 5,
+	  comments: 2,
+	  createdAt: "2 minutes ago",
+	  views: 59,
+	  id: 3,
+	},
+  ];
 export const trending = (req, res) => {
-	const videos = [
-		{
-		  title: "Hello",
-		  title: "First Video",
-		  rating: 5,
-		  comments: 2,
-		  createdAt: "2 minutes ago",
-		  views: 59,
-		  id: 1,
-		},
-		{
-		  title: "Video #2",
-		  title: "Second Video",
-		  rating: 5,
-		  comments: 2,
-		  createdAt: "2 minutes ago",
-		  views: 59,
-		  id: 1,
-		},
-		{
-		  title: "Whatsup",
-		  title: "Third Video",
-		  rating: 5,
-		  comments: 2,
-		  createdAt: "2 minutes ago",
-		  views: 59,
-		  id: 1,
-		},
-	  ];
-	res.render("home", {pageTitle : "Home", fakeUser : fakeUser, videos});
+	res.render("home", {pageTitle : "Home", videos});
 }
-export const watch = (req,res)=> res.render("watch", {pageTitle : "Watch this video"});
-export const edit = (req,res) => res.send("edit video");
+export const watch = (req,res)=> {
+	const { id } = req.params;
+	const video = videos[id-1];
+	return res.render("watch", {pageTitle : `watch ${video.title} video`, video});
+}
+export const getEdit = (req,res) => {
+	const { id } = req.params;
+	const video = videos[id-1];
+	return res.render("edit", {pageTitle : `Editing :${video.title}`, video})
+};
+export const postEdit = (req,res) => {
+	const { id } = req.params;
+	const { title } = req.body;
+	videos[id - 1].title = title; 
+	return res.redirect(`/video/${id}`);
+};
 export const search = (req, res) =>res.send("search video");
 export const remove = (req, res) => res.send("remove video");
 export const upload = (req ,res) => res.send("upload video");
--- src/server.js
+++ src/server.js
@@ -4,19 +4,16 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-
 const app = express();
 const PORT = 4000;
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 //app이 request에 어떻게 대응할 지를 정해준다.
-app.use(morgan("dev"));				//함수가 모든 url에 대해 middleware로 작용한다.
-
+app.use(morgan("dev")); //함수가 모든 url에 대해 middleware로 작용한다.
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
 
-
-
 //행동 양식이 모두 정해진 후, 서버를 열어 외부에 내보낸다.
-app.listen(PORT, ()=>console.log("Server listening on port 4000"));
+app.listen(PORT, () => console.log("Server listening on port 4000"));
